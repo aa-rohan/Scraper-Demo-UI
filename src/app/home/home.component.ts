47347b1562ae9f3d013a6051c00f215556b6afd6
Aarohan <aarohanthapa@bajratechnologies.com>
2024-06-06 19:33:32 +0545

    feat: display errors when scraping fails

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '../api.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, ProductListComponent, FormsModule],
+  imports: [AsyncPipe, ProductListComponent, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -19,13 +19,27 @@ export class HomeComponent {
 
   products$: Observable<any[]> = new Observable();
   productUrl: string | null = null;
+  showMessage: boolean = false;
+  notificationMessage: string = '';
 
   scrape() {
     if (!this.productUrl) {
       return
     }
+    this.displayMessage('Scraping Product...');
     this.api.scrapeProduct(this.productUrl).subscribe((product: any) => {
       this.router.navigate(['/product', product.data.id]);
+    }, (error: any) => {
+      this.displayMessage('There was an issue scraping the product.');
     });
+  }
+
+  private displayMessage(message: string): void {
+    this.notificationMessage = message;
+    this.showMessage = true;
+    setTimeout(() => {
+      this.notificationMessage = '';
+      this.showMessage = false;
+    }, 1000)
   }
 }
