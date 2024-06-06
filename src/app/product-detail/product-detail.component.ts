0e7579113f54982cc170d0bb57a3441901562e61
Aarohan <aarohanthapa@bajratechnologies.com>
2024-06-06 09:04:53 +0545

    feat: style product detail component

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,14 +1,14 @@
 import { AsyncPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
